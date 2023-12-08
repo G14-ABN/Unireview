6ad0ee45e7f4c74e760213c261793975f7b232c0
SashaBusinaro <businaro.sasha@gmail.com>
2023-12-08 17:25:57 +0100

    aggiunta dir models per gli schemi db e routes per le API

---
+++ source/backend/models/Review.js
@@ -0,0 +1,60 @@
+import mongoose from "mongoose";
+
+const reviewSchema = new mongoose.Schema({
+  autore: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User", // Fa riferimento al modello User
+    required: true,
+  },
+  professore: {
+    type: String,
+    required: true,
+  },
+  corso: {
+    type: String,
+    required: true,
+  },
+  data: {
+    type: Date,
+    default: Date.now,
+  },
+  valutazioneProfessore: {
+    type: Number,
+    min: 1,
+    max: 5,
+    required: true,
+  },
+  valutazioneFattibilita: {
+    type: Number,
+    min: 1,
+    max: 5,
+    required: true,
+  },
+  valutazioneMateriale: {
+    type: Number,
+    min: 1,
+    max: 5,
+    required: true,
+  },
+  testo: {
+    type: String,
+    default: null,
+  },
+  voto: {
+    type: Number,
+    default: null,
+  },
+  frequenza: {
+    type: String,
+    enum: ["Nessuna", "Rara", "Occasionale", "Frequente"],
+    default: "Nessuna",
+  },
+  anonima: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const Review = mongoose.model("Review", reviewSchema);
+
+export default Review;
