b440f80468450b5231ef2b68819244de1049175b
matteo23nanni <129533916+matteo23nanni@users.noreply.github.com>
2024-05-21 20:53:31 +0200

    Delete source/frontend/DB directory

--- source/frontend/DB/corsi.tsx
+++
@@ -1,8 +0,0 @@
-export {Corsi}
-
-class Corsi{
-    static corsi = ['Reti', 'Programmazione avanzata', 'Basi di dati',
-                    'Analisi 1', 'Sistemi informatvi', 'Programmazione 2',
-                    'Geometria', 'Fondamenti matematici']
-    //static getCorsi(){return this.corsi}
-}
--- source/frontend/DB/professori.tsx
+++
@@ -1,9 +0,0 @@
-export {Professors} 
-
-
-class Professors{
-    static professori= [ 'Bouquet', 'Patrignani', 'Casari','Marchese', 'Brunetti',
-        'Perotti', 'Ghiloni'
-    ]
-    //static getProfessori(){return this.professori}
-}
